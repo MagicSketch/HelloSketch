4256bd26607ec6ffd6e35e55c80823bfd65a34b3
James Tang <j@jamztang.com>
2017-01-27 14:16:25 +0800

    Initial Commit

---
+++ HelloSketch/HelloSketch.h
@@ -0,0 +1,19 @@
+//
+//  HelloSketch.h
+//  HelloSketch
+//
+//  Created by James Tang on 27/1/2017.
+//  Copyright © 2017 MagicSketch. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for HelloSketch.
+FOUNDATION_EXPORT double HelloSketchVersionNumber;
+
+//! Project version string for HelloSketch.
+FOUNDATION_EXPORT const unsigned char HelloSketchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelloSketch/PublicHeader.h>
+
+
---
+++ HelloSketchTests/HelloSketchTests.m
@@ -0,0 +1,39 @@
+//
+//  HelloSketchTests.m
+//  HelloSketchTests
+//
+//  Created by James Tang on 27/1/2017.
+//  Copyright © 2017 MagicSketch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloSketchTests : XCTestCase
+
+@end
+
+@implementation HelloSketchTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
